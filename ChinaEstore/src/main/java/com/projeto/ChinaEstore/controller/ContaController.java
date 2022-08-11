09270c8d0aea634e66cd7bfa71427289da1c6475
KevinBitencourt <kevin.bitencourt.silva@gmail.com.br>
2022-08-11 11:26:04 -0300

    Versão 11/08/22

--- ChinaEstore/src/main/java/com/projeto/ChinaEstore/controller/ContaController.java
+++ ChinaEstore/src/main/java/com/projeto/ChinaEstore/controller/ContaController.java
@@ -1,11 +1,17 @@
 package com.projeto.ChinaEstore.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.projeto.ChinaEstore.entidade.Conta;
@@ -17,6 +23,34 @@ import com.projeto.ChinaEstore.repository.ContaRepository;
 public class ContaController {
     @Autowired
     private ContaRepository contaRepository;
+
+    @PostMapping("/contas")
+    @ResponseBody
+    public Conta salvarConta(@RequestBody Conta conta){
+      contaRepository.save(conta);
+        return conta;
+    }
+    @GetMapping("/contas/lista")
+    @ResponseBody
+    public List<Conta> listarContas(){
+        List<Conta > ct = new ArrayList<>();
+       ct = contaRepository.findAll();
+        return ct;
+    }
+    @RequestMapping(path="/conta/delete/{Id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public String deletarContaPorId(@PathVariable ("Id")Long id){
+        contaRepository.deleteById(id);
+        return "Conta Deletada";
+    } 
+ 
+    @RequestMapping(path="/conta/lista/{Id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Optional<Conta> listaContaPorId(@PathVariable ("Id")Long id){
+       Optional<Conta> obj = contaRepository.findById(id);
+        return obj;
+    }
+
     @GetMapping("/conta/saldo/{id}")
     @ResponseBody
     public String saldoConta(@PathVariable("id")Long id){
--- ChinaEstore/src/main/java/com/projeto/ChinaEstore/controller/VendaController.java
+++ ChinaEstore/src/main/java/com/projeto/ChinaEstore/controller/VendaController.java
@@ -1,8 +1,51 @@
 package com.projeto.ChinaEstore.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.projeto.ChinaEstore.entidade.Venda;
+import com.projeto.ChinaEstore.repository.VendaRepository;
 
 @Controller
 public class VendaController {
-   // @Autowired
+   @Autowired
+   private VendaRepository vendaRepository;
+   @PostMapping("/vendas")
+   @ResponseBody
+   public Venda salvarVenda(@RequestBody Venda venda){
+     vendaRepository.save(venda);
+       return venda;
+   }
+   @GetMapping("/vendas/lista")
+   @ResponseBody
+   public List<Venda> listarVendas(){
+       List<Venda > vd = new ArrayList<>();
+      vd = vendaRepository.findAll();
+       return vd;
+   }
+   @RequestMapping(path="/venda/delete/{Id}", method = RequestMethod.DELETE)
+   @ResponseBody
+   public String deletarVendaPorId(@PathVariable ("Id")Long id){
+       vendaRepository.deleteById(id);
+       return "Venda Deletada";
+   }
+
+   @RequestMapping(path="/venda/lista/{Id}", method = RequestMethod.GET)
+   @ResponseBody
+   public Optional<Venda> listaVendaPorId(@PathVariable ("Id")Long id){
+      Optional<Venda> obj = vendaRepository.findById(id);
+       return obj;
+   }
+
 }
